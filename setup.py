e9206941b19ce04f18733aa0ef6040edbc67b518
Igor Starikov <idlesign@yandex.ru>
2013-07-12 08:43:58 +0600

    setup.py cleaned.

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup(
     url='http://github.com/idlesign/django-siteblocks',
     packages=['siteblocks'],
     include_package_data=True,
-    install_requires=['setuptools'],
     zip_safe=False,
     classifiers=[
 	'Development Status :: 4 - Beta',
