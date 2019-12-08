510c371120c13a65b104383bddc1355a738ca130
idle sign <idlesign@yandex.ru>
2019-12-08 09:34:43 +0700

    Add Django 3.0 compatibility

--- siteblocks/models.py
+++ siteblocks/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
 
 
-@python_2_unicode_compatible
 class Block(models.Model):
 
     alias = models.CharField(
